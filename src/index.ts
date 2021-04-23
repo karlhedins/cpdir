06b248ef4e28d725ee11cf5065b694af3aee4866
Karl Hedin <karl.hedin@jayway.com>
2021-04-23 12:11:28 +0200

    add docs and skip overwriting for copy

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import fs from 'fs-extra';
 
+const filterFn = (src: string, dest: string): boolean => {
+  console.log(`cpdir: ${src} -> ${dest}`);
+  // add filter logic here if you want to skip files/directories
+  // return true the files you want to keep
+  return true;
+};
+
 const cpdir = async () => {
   try {
-    await fs.copy('./testfolder1', './testfolder2');
+    await fs.copy('./testfolder1', './testfolder2', {
+      overwrite: false,
+      filter: filterFn,
+    });
     console.log('success!');
   } catch (err) {
     console.error(err);
